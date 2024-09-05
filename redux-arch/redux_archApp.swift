a8eb8ac40626556bb3586416babd00e3ddca5a8a
Kayode <kayodefredrickbalogun@gmail.com>
2024-09-05 11:43:23 +0100

    Initial Commit

---
+++ redux-arch/redux_archApp.swift
@@ -0,0 +1,17 @@
+//
+//  redux_archApp.swift
+//  redux-arch
+//
+//  Created by Balogun Kayode on 05/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct redux_archApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
